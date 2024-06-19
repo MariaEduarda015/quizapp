c716df198c88e8c71111c0b45f412f5c6c5f1dbc
Maria Eduarda Santana <mariaeduardasantanas04@gmail.com>
2024-06-19 18:57:20 +0000

    adicionada a função guardar resposta e o id resposta

--- pages/quiz/quiz.js
+++ pages/quiz/quiz.js
@@ -66,7 +66,7 @@ function montarPergunta(){
                     <form action="">
 
                       <label for="alternativa_a" >
-                        <input type="radio" id="alternativa_a" name="alternativa">
+                        <input type="radio" id="alternativa_a" name="alternativa" value=" ${alterarSinais(quiz.question[pergunta-1].options[0])}">
                         <div>
                             <span>A</span>
                            ${alterarSinais(quiz.question[pergunta-1].options[0])}
@@ -75,7 +75,7 @@ function montarPergunta(){
 
 
                       <label for="alternativa_b" >
-                        <input type="radio" id="alternativa_b" name="alternativa">
+                        <input type="radio" id="alternativa_b" name="alternativa" value"  ${alterarSinais(quiz.question[pergunta-1].options[1])}">
                         <div>
                             <span>B</span>
                              ${alterarSinais(quiz.question[pergunta-1].options[1])}
@@ -84,7 +84,7 @@ function montarPergunta(){
 
 
                       <label for="alternativa_c">
-                        <input type="radio" id="alternativa_c" name="alternativa">
+                        <input type="radio" id="alternativa_c" name="alternativa" valeu" ${alterarSinais(quiz.question[pergunta-1].options[2])}">
                         <div>
                             <span>C</span>
                             ${alterarSinais(quiz.question[pergunta-1].options[2])}
@@ -93,7 +93,7 @@ function montarPergunta(){
 
 
                       <label for="alternativa_d">
-                        <input type="radio" id="alternativa_d" name="alternativa">
+                        <input type="radio" id="alternativa_d" name="alternativa" valeu"${alterarSinais(quiz.question[pergunta-1].options[3])}">
                         <div>
                             <span>D</span>
                              ${alterarSinais(quiz.question[pergunta-1].options[3])}
@@ -111,10 +111,20 @@ function alterarSinais(texto) {
   return texto.replace(/>/g, "&lt").replace(/>/g, "&gt;")
 }
 
+function guardarResposta(){
+  console.log("guardar resposta")
+
+}
+
 async function iniciar(){
   alterarAssunto()
   await buscarPerguntas()
   montarPergunta()
+
+  const inputResposta = document.querySelectorAll(".altenativas input")
+  inputResposta.forEach(input => {
+    input.addEventListener("click", guardarResposta)
+  })
 
 }
 
